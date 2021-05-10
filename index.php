79a5992b1c0dbddee828ca220d9e50a13f7376af
anibalcoral <83928278+anibalcoral@users.noreply.github.com>
2021-05-09 23:50:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-type: text/plain');
-echo "Anibal Coral";
 echo "\n\nHostName: " . gethostname(); 
 echo "\n\nServidor HTTP: " . $_SERVER['HTTP_USER_AGENT'];
 echo "\n\nUname: " . php_uname('n');
 echo "\n\nBrowser:" . $_SERVER['HTTP_USER_AGENT'] . "\n\n";
 echo "\n\n\nFuncionou !!!!";
+echo "\n\nAnibal Coral";
 
 ?>
