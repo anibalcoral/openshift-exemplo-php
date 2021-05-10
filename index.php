00788ca897815235f6467cacb0b3ee4395305c70
anibalcoral <83928278+anibalcoral@users.noreply.github.com>
2021-05-09 22:29:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Demonstração OpenShift - RedHat</title>
+ </head>
+ <body>
+ <p>Olá Mundo\n</p>
+ <p>Anibal Ulisses Coral\n</p>
+ <p>Informações PHP:\n</p>
+ <?php phpinfo(); ?>
+ </body>
+</html>
+
